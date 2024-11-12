7b57f0ba302ac7a7876fbd0c9c3d7ec4ba16d045
Edugonzalez <eduu.gonzalez@gmail.com>
2024-11-12 19:32:42 +0100

    Ejercicio 11 completado. En el tvRemote.java incluí 2 setters para establecer el volumen y el canal a voluntad; y en el main unos cuantos Sout para ir haciendo comprobaciones.

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -4,8 +4,44 @@ public class Exercise11 {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
+
+        redRemote.turnOn();
+        System.out.println(redRemote.on);
+
+        redRemote.channelUp();
+        System.out.println(redRemote.channel);
+
+        redRemote.channelDown();
+        System.out.println(redRemote.channel);
+
+        redRemote.setChannel(0);
+        System.out.println(redRemote.channel);
+
+        redRemote.channelDown();
+        System.out.println(redRemote.channel);
+
+        redRemote.setVolume(0);
+        redRemote.volumeDown();
+        System.out.println(redRemote.volume);
+
         System.out.println(redRemote.getColor());
+
+        redRemote.turnOff();
+        System.out.println(redRemote.on);
+
+        /*
+    Encender la TV
+    Subir un canal
+    Bajar un canal
+    Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+    Subir el volumen
+    Bajar el volumen
+    Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+    Obtener el color del mando a distancia y mostrarlo por pantalla
+     System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+    Apagar la TV
+    */
     }
 
 }
